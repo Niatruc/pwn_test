2748fb49f3a023abb3289a24eae1f37355527c26
zbh <810146800@qq.com>
2021-08-16 09:50:15 +0800

    Delete a.py
    .

--- a.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os, sys
-if __name__ == "__main__":
-    print("Try commands below")
-    print("$ echo 'foobar' > /proc/{0}/fd/0".format(os.getpid()))
-    while True:
-        print("read :: [" + sys.stdin.readline() + "]")
-        pass
